b62561f965e537cf3294c55efc1e8c3361d570d5
Nhv24 <69376044+Nhv24@users.noreply.github.com>
2021-07-02 11:13:17 -0600

    Fix indents

--- server.js
+++ server.js
@@ -10,10 +10,10 @@ const HISTORY = {};
 const users = [];
 
 const adduser = (id, name, room) => {
-    if (users.find(user => user.name.trim().toLowerCase().replace(/[\u200B-\u200D\uFEFF]/g, '') === name.trim().toLowerCase().replace(/[\u200B-\u200D\uFEFF]/g, ''))) {
+    	if (users.find(user => user.name.trim().toLowerCase().replace(/[\u200B-\u200D\uFEFF]/g, '') === name.trim().toLowerCase().replace(/[\u200B-\u200D\uFEFF]/g, ''))) {
 		return {error: "Username is already taken"};
 	}
-    else if (!name || !room || name === "" || room === '') {
+    	else if (!name || !room || name === "" || room === '') {
 		return {error: "Username and room are required!"};
 	}
 
@@ -24,8 +24,8 @@ const adduser = (id, name, room) => {
 	else if (users.length >= 50) {
 		return {error: "Sorry, but the maximum number of users was bypassed. Please check back later."}
 	}
-    users.push({name, room, id});
-    return {name, room, id};
+    	users.push({name, room, id});
+    	return {name, room, id};
 }
 
 const clean = (string) => {
@@ -48,9 +48,9 @@ app.get("/", (req, res) => {
 });
 
 io.on('connection', (socket) => {
-    socket.on('chat', (msg) => {
+	socket.on('chat', (msg) => {
 		msg = clean(msg);
-        const user = users.find(uname => uname.id === socket.id);
+        	const user = users.find(uname => uname.id === socket.id);
 
 		if (user) {
 			if (!HISTORY[user.room]) {
@@ -62,29 +62,29 @@ io.on('connection', (socket) => {
 					HISTORY[user.room].shift();
 				}
 			}
-        	io.to(user.room).emit('message', {name: clean(user.name), text: msg});
+        		io.to(user.room).emit('message', {name: clean(user.name), text: msg});
 		}
   	});
 
 	socket.on('login', data => {
 		const user = adduser(socket.id, clean(data.name), data.room);
-        if (user.error) {
+        	if (user.error) {
 			socket.emit("error", user.error);
 		}
 		else {
-        	socket.join(data.room);
-        	io.to(data.room).emit('join', {name: clean(data.name), users: getusers(data.room), history: HISTORY[data.room]});
+        		socket.join(data.room);
+        		io.to(data.room).emit('join', {name: clean(data.name), users: getusers(data.room), history: HISTORY[data.room]});
 		}
    	});
 
-    socket.on("disconnect", () => {
-        const index = users.findIndex(user => user.id === socket.id);
-    	if (index !== -1) {
+    	socket.on("disconnect", () => {
+        	const index = users.findIndex(user => user.id === socket.id);
+    		if (index !== -1) {
 			const data =  users.splice(index, 1)[0];
 			socket.leave(data.room);
-            io.to(data.room).emit('exit', {name: data.name, users: getusers(data.room)});
+        		io.to(data.room).emit('exit', {name: data.name, users: getusers(data.room)});
 		}
-    });
+    	});
 });
 
 http.listen(process.env.PORT || 3000);
